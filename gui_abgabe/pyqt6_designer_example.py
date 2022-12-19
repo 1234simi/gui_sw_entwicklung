28f67aec584efd62df0eb2624e04eedc01165ae4
DodoMels <107034626+DodoMels@users.noreply.github.com>
2022-12-19 14:34:33 +0100

    check comment

--- gui_abgabe/pyqt6_designer_example.py
+++ gui_abgabe/pyqt6_designer_example.py
@@ -3,7 +3,7 @@ from PyQt5 import QtWidgets, uic
 from PyQt5.QtWidgets import QMainWindow, QApplication, QDialog
 
 from Mandelbrot.run_video import main
-
+# check github
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
