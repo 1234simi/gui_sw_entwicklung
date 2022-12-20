2402918bf0b69a14f0b766d9785770396994eb8f
DodoMels <107034626+DodoMels@users.noreply.github.com>
2022-12-20 16:00:46 +0100

    Creat an Icon and load a picture into the graphicsview

--- gui_abgabe/pyqt6_designer_example.py
+++ gui_abgabe/pyqt6_designer_example.py
@@ -1,9 +1,12 @@
 import sys
 from PyQt5 import QtWidgets, uic
+from PyQt5.QtGui import QIcon, QPixmap
 from PyQt5.QtWidgets import QMainWindow, QApplication, QDialog
+from PyQt5.QtCore import *
+
 
 from Mandelbrot.run_video import main
-# check github
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -15,13 +18,23 @@ class MainWindow(QMainWindow):
         # Widgets from MainWIndow
         # Wenn der 'pushButton' geklickt wird
         self.pushButton.clicked.connect(self.handleButtonClick)
-
+        self.setWindowTitle("Mandelbrot Zoom")
+        self.setWindowIcon(QIcon("Icon.jpg"))  # Place an icon and import QIcon
 
         # External windows
         self.dialogWindow = DialogWindow()
         self.dialogWindow.pushButton.clicked.connect(self.dialogWindow.close)
-
         self.show()
+
+        # load the Picture into the Graphicsview
+        pix = QPixmap('Icon.jpg')
+        item = QtWidgets.QGraphicsPixmapItem(pix)
+        scene = QtWidgets.QGraphicsScene(self)
+        scene.addItem(item)
+        self.graphicsView.setScene(scene)
+
+
+
 
     def handleButtonClick(self):
         self.dialogWindow.show()
@@ -30,6 +43,7 @@ class DialogWindow(QDialog):
     def __init__(self):
         super().__init__()
         uic.loadUi("dialog.ui", self)
+        self.setWindowIcon(QIcon("Icon.jpg"))  # Place an icon and import QIcon
         # uic.loadUi("gui_data.ui", self)
 
 
